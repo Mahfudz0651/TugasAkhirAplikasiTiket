fc09921f3f433b229c00ea6e545534d0d7178ebe
TC1034 <mahfudzsiddiq0@gmail.com>
2024-01-10 21:26:36 +0700

    Edit String

--- app/src/main/java/com/example/aplikasitiket/MainActivity.kt
+++ app/src/main/java/com/example/aplikasitiket/MainActivity.kt
@@ -1,50 +1,26 @@
 package com.example.aplikasitiket
 
-import android.app.Activity
-import android.graphics.Color
-import android.os.Build
 import android.os.Bundle
-import android.view.View
-import android.view.WindowManager
-import androidx.appcompat.app.AppCompatActivity
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.example.aplikasitiket.ui.theme.AplikasiTiketTheme
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            AplikasiTiketTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
 
-        setStatusBar()
-
-
-    }
-
-
-    private fun setStatusBar() {
-        if (Build.VERSION.SDK_INT < 21){
-            setWindowFlag(this, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, true)
-        }
-        if (Build.VERSION.SDK_INT >=19){
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                window.decorView.systemUiVisibility =
-                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_FULLSCREEN
+                }
             }
-        }
-        if (Build.VERSION.SDK_INT >= 21) {
-            setWindowFlag(this, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, false)
-            window.statusBarColor = Color.TRANSPARENT
-        }
-    }
-
-    companion object {
-        fun setWindowFlag(activity: Activity, bits: Int, on: Boolean){
-            val window = activity.window
-            val layoutParams = window.attributes
-            if (on) {
-                layoutParams.flags = layoutParams.flags or bits
-            } else {
-                layoutParams.flags = layoutParams.flags and bits.inv()
-            }
-            window.attributes = layoutParams
         }
     }
 }
