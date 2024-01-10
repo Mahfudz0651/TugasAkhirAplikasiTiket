805673450aaf121f40bd34cb837aa4bcebafcb61
TC1034 <mahfudzsiddiq0@gmail.com>
2024-01-10 21:46:10 +0700

    Create SumberData

---
+++ app/src/main/java/com/example/aplikasitiket/database/SumberData.kt
@@ -0,0 +1,13 @@
+package com.example.aplikasitiket.database
+
+import com.example.aplikasitiket.R
+
+object SumberData {
+    val Bus = listOf(
+        R.string.sinarjaya,
+        R.string.agramas,
+        R.string.haryanto,
+        R.string.budiman,
+        R.string.murnijaya
+    )
+}
