5d76b776e7bcfb73935823efddd8aeddd1871634
TC1034 <mahfudzsiddiq0@gmail.com>
2024-01-10 21:40:18 +0700

    Create OrderUiState

---
+++ app/src/main/java/com/example/aplikasitiket/database/OrderUIState.kt
@@ -0,0 +1,10 @@
+package com.example.aplikasitiket.database
+
+data class OrderUIState(
+    val jumlah: Int = 0,
+    val kelas: String = "",
+    val harga: String = "",
+    val nama: String = "",
+    val noTelp: String = "",
+    val Tujuan: String = "",
+)
